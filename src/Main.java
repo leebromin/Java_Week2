9ddf29d029e647077334fbafc83e7769c8d43d49
이형민 <mini100142@gmail.com>
2024-09-11 17:20:39 +0900

    20240911

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        /*
+        System.out.println("Hello, World2!");
+        System.out.println("Hello, World2!");
+         */
+    }
+}
---
+++ src/QuestionWeek2.java
@@ -0,0 +1,7 @@
+public class QuestionWeek2 {
+    public static void main(String[] args) {
+    char letter = 'A';
+    int asciiValue = letter;
+        System.out.println("The ASCII value of " + letter + " is " + asciiValue);
+    }
+}
